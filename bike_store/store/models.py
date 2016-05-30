ea173ae4bbb30e9ce2c81e8debcddaab606cc4a7
Pavel Karateev <karateev.pavel@ya.ru>
2016-05-30 12:18:38 +0300

    Model and migration

--- bike_store/store/models.py
+++ bike_store/store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class SparePart(models.Model):
+
+    name = models.CharField(max_length=100)
+    brand = models.ForeignKey(Brand)
+    price = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    contact = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
