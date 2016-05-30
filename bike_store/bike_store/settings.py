aa44f6dc383727824729d1686a5ce2b4f3013f9d
Pavel Karateev <karateev.pavel@ya.ru>
2016-05-30 12:02:28 +0300

    SECRET_KEY into environment variable

--- bike_store/bike_store/settings.py
+++ bike_store/bike_store/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        raise ImproperlyConfigured('Set the %s environment variable!' % var_name)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z)bvej^e3r23wrg&l7b0!r0p1y%5!*3@&c+-66&)p816yic^h!'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
